9ee973cce194adfc9650383cfadc752a49e36383
Renks <renks@localhost.localdomain>
2025-06-18 14:03:38 -0300

    feat: StealthToolkit class

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,6 @@
 from src.utils.human import HumanBehaviorSimulator, HumanMouseMover
+from src.utils.stealth import StealthToolkit
 from src.utils.chrome import ChromeManager
 from src.utils.scraper import SnSimei
 
-__all__ = ["HumanBehaviorSimulator", "HumanMouseMover", "ChromeManager", "SnSimei"]
+__all__ = ["HumanBehaviorSimulator", "HumanMouseMover", "StealthToolkit", "ChromeManager", "SnSimei"]
